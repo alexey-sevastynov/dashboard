5ebee91dffbaddf2df6702f6b24047fd59994b7e
Alexey Sevastynov <97807120+alexey-sevastynov@users.noreply.github.com>
2023-05-10 22:52:06 +0300

    adaptive side panel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import "./App.css";
 import SidePanel from "./components/side-panel/SidePanel";
+import SidePanelMob from "./components/side-panel-mob/SidePanelMob";
 
 function App() {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(max-width: 1400px)",
+  });
+
   return (
     <div className="container">
       <div className="wrapper">
+        {isDesktopOrLaptop && <SidePanelMob />}
         <SidePanel />
       </div>
     </div>
